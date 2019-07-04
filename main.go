f0af3e08a473f9f39a5b552a5e253d38a57d1f52
Satish Dasi <satish.dasi@gmail.com>
2019-07-04 20:59:18 +0530

    Change the message text.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "This is satish."}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
